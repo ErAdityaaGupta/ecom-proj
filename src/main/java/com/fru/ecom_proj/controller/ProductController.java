1c3e4d830aaa6471e684cbdfd7ae6f495348e62e
aditya <ingenious.adig@gmail.com>
2024-08-20 19:41:20 +0530

    Initial commit

--- src/main/java/com/fru/ecom_proj/controller/ProductController.java
+++ src/main/java/com/fru/ecom_proj/controller/ProductController.java
@@ -1,14 +1,28 @@
 package com.fru.ecom_proj.controller;
 
+import com.fru.ecom_proj.model.Product;
+import com.fru.ecom_proj.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class ProductController {
 
+    @Autowired
+    private ProductService service;
+
     @RequestMapping("/")
     public String greet(){
         return "Hello Aditya";
+    }
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts(){
+        return service.getAllProducts();
     }
 }
