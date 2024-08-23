9db6903033aa96b24eb04d917562eab02a99c63b
aditya <ingenious.adig@gmail.com>
2024-08-23 14:48:08 +0530

    Initial commit

--- src/main/java/com/fru/ecom_proj/controller/ProductController.java
+++ src/main/java/com/fru/ecom_proj/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.fru.ecom_proj.model.Product;
 import com.fru.ecom_proj.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -52,6 +53,16 @@ public class ProductController {
         } catch (Exception e) {
              return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping("/products/{productId}/image")
+    public ResponseEntity<byte[]> getImageByProductId(@PathVariable int productId){
+        Product product = service.getProductById(productId);
+        byte[] imageFile = product.getImageData();
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf(product.getImageType()))
+                .body(imageFile);
     }
 
 }
