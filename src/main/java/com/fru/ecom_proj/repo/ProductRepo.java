23628d1e3840ee5b8bd271f5c0293d1f88adb1de
aditya <ingenious.adig@gmail.com>
2024-08-23 18:16:53 +0530

    Initial commit

--- src/main/java/com/fru/ecom_proj/repo/ProductRepo.java
+++ src/main/java/com/fru/ecom_proj/repo/ProductRepo.java
@@ -4,8 +4,12 @@ import com.fru.ecom_proj.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepo extends JpaRepository<Product,Integer> {
+    // this kind of methods could be written for all types on which we want to search in the website.
+//    public List<Product> findByBrand(String Brand);
 
-
+    // This kind of thing could also be done with the help of JPQL  (Java Persistence Quarry Language)
 }
